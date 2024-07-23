8b8b9da336518931b455efd89a46332096849fa8
harshtrivedi312 <bluefishartandanimation@gmail.com>
2024-07-22 23:20:02 -0400

    auth 2

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import {Amplify} from 'aws-amplify';
 import awsconfig from './aws-exports';
 import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure({awsconfig})
-
-// Custom CSS
 import './App.css';
+import awsExports from "./aws-exports";
+
+Amplify.configure({awsconfig,
+    ...awsExports,
+    loggingLevel:'DEBUG'})
+
+
+
 
 
 
